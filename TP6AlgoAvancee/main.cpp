158645489fdd748521e98afee5f2c8a8cd9168d7
Hadrien62 <hadrien.delobel@student.junia.com>
2023-03-15 08:02:51 +0100

    Initial Commit

---
+++ TP6AlgoAvancee/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TP6AlgoAvancee
+//
+//  Created by Hadrien Delobel on 15/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
